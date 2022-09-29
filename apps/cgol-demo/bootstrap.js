f7ea975937f51e86f2b4a1b1daa6c7b163086463
Guanzhou Hu <huguanzhou123@sina.com>
2022-09-28 20:58:18 -0500

    update cgol demo styling

--- apps/cgol-demo/bootstrap.js
+++ apps/cgol-demo/bootstrap.js
@@ -174,7 +174,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/bitaggy_bg.wasm":"891d18e3c5942c05e99e"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/bitaggy_bg.wasm":"ee4448bba1bf20bf76bd"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
